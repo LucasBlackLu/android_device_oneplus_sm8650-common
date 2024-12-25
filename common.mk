e78bbdf5b3073a3fc9ef78f8e2fd3e1843742817
LuK1337 <priv.luk@gmail.com>
2024-12-25 23:42:18 +0100

    sm8550-common: Switch to NCM USB tethering
    Change-Id: I20b6b2b2e26a45d8b28c7483afc83ad4da4b8643

--- common.mk
+++ common.mk
@@ -275,6 +275,7 @@ PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     DeviceAsWebcamResTarget \
     FrameworksResTarget \
+    NcmTetheringOverlay \
     OPlusFrameworksResCommon \
     OPlusSettingsResCommon \
     OPlusSystemUIResCommon \
