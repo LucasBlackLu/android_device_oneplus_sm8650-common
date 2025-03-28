d4a239d471e3aea9d160af7935b04c904cdd3b61
chandu078 <chandudyavanapelli03@gmail.com>
2025-03-28 10:51:15 +0530

    Revert "[DNM]sm8650-common: Downgrade kernel compiler to Clang 17"
    This reverts commit ff616f909bfe1bd135f320deb6021e7a15a07e32.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -109,8 +109,6 @@ BOARD_BOOTCONFIG := \
     androidboot.vendor.qspa=true \
     androidboot.usbcontroller=a600000.dwc3
 
-TARGET_KERNEL_CLANG_VERSION := r487747c
-
 BOARD_USES_GENERIC_KERNEL_IMAGE := true
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
